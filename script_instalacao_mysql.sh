c63ad7c0774a71260de2cdeaf2d509a3b07ad380
Leandro Coelho Saraiva <72211521+lean-dro@users.noreply.github.com>
2023-10-21 03:07:18 -0300

    Update script_instalacao_mysql.sh

--- script_instalacao_mysql.sh
+++ script_instalacao_mysql.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-wget -O banco "https://raw.githubusercontent.com/Kyvo-io/scripts/main/monitro_script.sql"
-sudo apt install mysql-server
-cat banco | sudo mysql
+wget -O banco "https://raw.githubusercontent.com/Kyvo-io/scripts/main/monitro_script.sql" && sudo apt install mysql-server && cat banco | sudo mysql
