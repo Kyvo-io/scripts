abc4f1f1cf17a58dff0e6ab329b6c5088b82cc92
Ubuntu <ubuntu@ip-172-31-81-81.ec2.internal>
2023-11-13 23:54:25 +0000

    Atualização do script

--- ScriptInstalação.sh
+++ ScriptInstalação.sh
@@ -1,10 +1,12 @@
+echo "Atualizando pacotes para instalação da aplicação"
 sudo apt update & sudo apt upgrade -y
-sudo adduser monitro-user
-sudo usermod -a -G sudo monitro-user
-sudo su monitro-user
 
- cd monitro-user
+sudo service mysql stop
+cd ~
 
+echo "Instalação dos arquivos e dependências necessárias"
+
+rm -d -rf scripts
 git clone https://github.com/Kyvo-io/scripts.git
 cd scripts
 
@@ -13,9 +15,12 @@ sudo apt-get install docker.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
 
-sudo curl -SL https://github.com/docker/compose/releases/download/v2.23.0/dockercompose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 sudo docker build -t monitro/monitro-banco:1.0 .
 
 sudo docker compose up --detach
+
+java -jar login-monitro.jar
 
