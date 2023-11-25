e4373e281ce012dd38e3c06e9d43234dfd565394
Leandro Coelho Saraiva <72211521+lean-dro@users.noreply.github.com>
2023-11-25 03:34:11 -0300

    Update run_docker_jar.sh

--- run_docker_jar.sh
+++ run_docker_jar.sh
@@ -1,3 +1,5 @@
+docker container rm $(docker container ls -aq) --force
+docker rmi $(docker image ls -aq) --force
 sudo docker build -t monitro/monitro-banco:1.0 .
 sleep 5
 sudo docker compose up --detach
