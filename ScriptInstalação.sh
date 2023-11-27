f4ab03ba6494f7af7b83e1ce1d04fd5e05a16b87
carlosgoes0 <carlos.goes@sptech.school>
2023-11-27 17:50:35 -0300

    Menu adicionado ao Script

--- ScriptInstalação.sh
+++ ScriptInstalação.sh
@@ -1,19 +1,48 @@
-echo "Atualizando pacotes para instalação da aplicação"
-sudo apt update & sudo apt upgrade -y
+2#!/bin/bash
 
-sudo service mysql stop
-cd ~
-rm -d -rf scripts
-sleep 5
-echo "Instalação dos arquivos e dependências necessárias"
-git clone https://github.com/Kyvo-io/scripts.git
-cd scripts
-sudo apt-get install docker.io -y
-sudo systemctl start docker
-sudo systemctl enable docker
-sleep 5
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sleep 5
-bash run_docker_jar.sh
+while true; do
+    echo "Bem-vindo à instalação da aplicação!"
+    echo "------------------------------"
+    echo "1. Instalar atualizações, arquivos e dependências"
+    echo "2. Instalar Docker e Docker Compose"
+    echo "3. Executar aplicação"
+    echo "4. Sair"
+    echo "------------------------------"
 
+    read -p "Por favor, selecione uma opção (1-4): " choice
+
+    case $choice in
+        1)
+            echo "Atualizando pacotes..."
+            sudo apt update && sudo apt upgrade -y
+            echo "Parando o serviço MySQL..."
+            sudo service mysql stop
+            echo "Baixando arquivos e dependências..."
+            cd ~
+            rm -d -rf scripts
+            sleep 5
+            git clone https://github.com/Kyvo-io/scripts.git
+            cd scripts
+            ;;
+        2)
+            echo "Instalando Docker e Docker Compose..."
+            sudo apt-get install docker.io -y
+            sudo systemctl start docker
+            sudo systemctl enable docker
+            sleep 5
+            sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+            sudo chmod +x /usr/local/bin/docker-compose
+            ;;
+        3)
+            echo "Executando a aplicação..."
+            bash run_docker_jar.sh
+            ;;
+        4)
+            echo "Saindo..."
+            exit 0
+            ;;
+        *)
+            echo "Opção inválida. Por favor, escolha uma opção válida."
+            ;;
+    esac
+done
--- run_docker_jar.sh
+++ run_docker_jar.sh
@@ -1,8 +1,23 @@
-docker container rm $(docker container ls -aq) --force
+#!/bin/bash
+
+echo "Preparando o ambiente para a aplicação..."
+echo "------------------------------"
+
+echo "Removendo containers existentes..."
+sudo docker container rm $(sudo docker container ls -aq) --force
 sleep 5
+
+echo "Construindo a imagem do banco de dados..."
 sudo docker build -t monitro/monitro-banco:1.0 .
 sleep 5
-sudo docker compose up --detach
-sudo docker compose up -d
+
+echo "Iniciando os serviços..."
+sudo docker-compose up --detach
+# ou, se preferir
+# sudo docker-compose up -d
 sleep 5
+
+echo "Executando a aplicação..."
 java -jar login-monitro.jar
+
+echo "Ambiente configurado com sucesso!"
