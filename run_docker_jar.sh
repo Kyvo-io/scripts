e50ba0339fb5b78b984c0e73087fa61158cb9fd2
Carlos Goes <125396028+carlosgoes0@users.noreply.github.com>
2023-11-29 17:50:18 -0300

    Update run_docker_jar.sh

--- run_docker_jar.sh
+++ run_docker_jar.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
+echo "*--------------------------------------------*"
+echo "| Preparando o ambiente para a aplicação... |"
+echo "*--------------------------------------------*"
 
-echo "Preparando o ambiente para a aplicação..."
-echo "------------------------------"
+echo "*------------------------------------*"
+echo "| Removendo containers existentes... |"
+echo "*------------------------------------*"
 
-echo "Removendo containers existentes..."
 sudo docker container rm $(sudo docker container ls -aq) --force
 sleep 5
 
-echo "Construindo a imagem do banco de dados..."
+echo "*-------------------------------------------*"
+echo "| Construindo a imagem do banco de dados... |"
+echo "*-------------------------------------------*"
+
 sudo docker build -t monitro/monitro-banco:1.0 .
 sleep 5
+echo "*---------------------------*"
+echo "| Iniciando os serviços...  |"
+echo "*---------------------------*"
 
-echo "Iniciando os serviços..."
 sudo docker-compose up --detach
 # ou, se preferir
 # sudo docker-compose up -d
 sleep 5
+echo "*---------------------------*"
+echo "| Executando a aplicação... |"
+echo "*---------------------------*"
 
-echo "Executando a aplicação..."
 java -jar login-monitro.jar
 
-echo "Ambiente configurado com sucesso!"
+echo "*-----------------------------------*"
+echo "| Ambiente configurado com sucesso! |"
+echo "*-----------------------------------*"
+
